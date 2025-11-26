240720735b92b0ef913ab0fcc542cbd5eef32e2d
Kevin Santana <kam.santana@yavirac.edu.ec>
2025-11-25 23:49:29 -0500

    clase 18 final

--- src/app/admin/inventario/services/categoria.service.ts
+++ src/app/admin/inventario/services/categoria.service.ts
@@ -26,19 +26,12 @@ export class CategoriaService {
   funGuardar(registro: any){
     return this.http.post(`${this.baseUrl}/categoria`, registro)
   }
-<<<<<<< HEAD
+
   funModificar(id: number, registro: any){
     return this.http.put(`${this.baseUrl}/categoria/${id}`, registro)
   }
+
   funEliminar(id: number){
-=======
-
-  funModificar (id: number, registro: any){
-    return this.http.patch(`${this.baseUrl}/categoria/${id}`,registro)
-  }
-
-  funEliminar (id:number){
->>>>>>> 521ca955105692b82566d6ab14637767f016b853
     return this.http.delete(`${this.baseUrl}/categoria/${id}`)
   }
 }
